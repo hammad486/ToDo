4f53262c608feefc753c5c908985eea4cc28df58
EHSAN COMPUTER <EHSAN COMPUTER@DESKTOP-KG25HVC>
2024-12-31 02:14:25 -0800

    Add project files.

---
+++ ToDo/Services/TaskServices.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Services
+{
+    public class TaskServices
+    {
+        
+        private readonly AppDbContext _context;
+
+        public TaskServices(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<TodoTask>> GetTasksAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task AddTaskAsync(TodoTask task)
+        {
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateTaskAsync(TodoTask task)
+        {
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
